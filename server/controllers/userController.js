6ccec38ec71aab7443f21a869bfec192fcd0c1e8
Krishna Bata <tusharbata12345@gmail.com>
2026-02-28 12:06:19 +0530

    initial commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,85 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import Chat from "../models/Chat.js";
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+// API to Register User
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const user = await User.create({ name, email, password });
+
+    const token = generateToken(user._id);
+
+    return res.json({ success: true, token });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to Login User
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        const token = generateToken(user._id);
+        return res.json({ success: true, token });
+      }
+    }
+    return res.json({ success: false, message: "Invalid email or password" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get User Data
+export const getUser = async (req, res) => {
+  try {
+    const user = req.user;
+    return res.json({ success: true, user });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get Published Images
+export const getPublishedImages = async (req, res) => {
+  try {
+    const publishedImagesMessages = await Chat.aggregate([
+      { $unwind: "messages" },
+      {
+        $match: {
+          "messages.isImage": true,
+          "messages.isPublished": true,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          imageUrl: "messages.content",
+          userName: "$userName",
+        },
+      },
+    ]);
+    return res.json({
+      success: true,
+      images: publishedImagesMessages.reverse(),
+    });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
